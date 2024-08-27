ae19b369cd2e382def8c9fb0875cf80cb5497952
Rimantas <belauzas@gmail.com>
2024-08-27 13:32:40 +0300

    Update

--- pages/pageViewAllAccounts.js
+++ pages/pageViewAllAccounts.js
@@ -2,18 +2,16 @@ import { footer } from "../components/footer.js";
 import { head } from "../components/head.js";
 import { header } from "../components/header.js";
 
-export function page404() {
+export function pageViewAllAccounts() {
     return `
         <!DOCTYPE html>
         <html lang="en">
-        ${head('404', ['main', 'header'])}
+        ${head('Visos paskyros', ['main', 'header'])}
         <body>
-            ${header()}
+            ${header('/accounts')}
 
             <main>
-                <h1>404</h1>
-                <h2>Norimas puslapis nerastas</h2>
-                <p>Eiti i <a href="/">pagrindini puslapi</a></p>
+                <h1>Visos paskyros</h1>
             </main>
 
             ${footer()}
