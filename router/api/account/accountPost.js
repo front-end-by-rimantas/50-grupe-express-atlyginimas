1e9fd7f419326ac6f5c93c2f2e25eab1085ab2d7
Rimantas <belauzas@gmail.com>
2024-08-29 14:37:57 +0300

    Validations

--- router/api/account/accountPost.js
+++ router/api/account/accountPost.js
@@ -1,15 +1,42 @@
 import { accountsData } from '../../../data/accountsData.js';
+import { isName } from '../../../lib/validation.js';
 
 export function accountPost(req, res) {
-    if (typeof req.body !== 'object') {
+    if (typeof req.body !== 'object'
+        || Array.isArray(req.body)
+        || req.body === null) {
         return res.json({
             status: 'error',
             message: 'Netinkamas duomenų tipas, turi būti objektas',
         });
     }
 
-    // tuscias objektas
-    // if ()
+    const name = req.body.name;
+    const nameError = isName(name);
+    if (nameError !== '') {
+        return res.json({
+            status: 'error',
+            message: nameError,
+        });
+    }
+
+    const date = req.body.date;
+    // const dateError = isDate(date);
+    // if (dateError !== '') {
+    //     return res.json({
+    //         status: 'error',
+    //         message: dateError,
+    //     });
+    // }
+
+    const rate = req.body.rate;
+    // const rateError = isRate(rate);
+    // if (rateError !== '') {
+    //     return res.json({
+    //         status: 'error',
+    //         message: rateError,
+    //     });
+    // }
 
     accountsData.push({
         ...req.body,
