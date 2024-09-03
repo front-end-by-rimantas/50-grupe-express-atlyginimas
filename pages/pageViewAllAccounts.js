848f9f6523518380891a7f8989358ad3404a0664
Rimantas <belauzas@gmail.com>
2024-09-03 13:23:35 +0300

    brains is warming

--- pages/pageViewAllAccounts.js
+++ pages/pageViewAllAccounts.js
@@ -2,8 +2,12 @@ import { footer } from "../components/footer.js";
 import { head } from "../components/head.js";
 import { header } from "../components/header.js";
 import { accountsData } from "../data/accountsData.js";
+import { workingDaysCount } from "../lib/helpers.js";
 
 export function pageViewAllAccounts() {
+    const previousMonth = workingDaysCount(2024, 8);
+    const currentMonth = workingDaysCount(2024, 9);
+
     return `
         <!DOCTYPE html>
         <html lang="en">
@@ -23,7 +27,8 @@ export function pageViewAllAccounts() {
                             <td>Vardas</td>
                             <td>Isidarbinimo data</td>
                             <td>Valandinis</td>
-                            <td>Atlyginimas</td>
+                            <td>Praeitas menuo</td>
+                            <td>Einamasis menuo</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -33,7 +38,8 @@ export function pageViewAllAccounts() {
                                 <td>${item.name}</td>
                                 <td>${item.date}</td>
                                 <td>${item.rate}</td>
-                                <td>${item.salary}</td>
+                                <td>${previousMonth} d.d. (${item.rate * previousMonth * 8} Eur)</td>
+                                <td>${currentMonth} d.d. (${item.rate * currentMonth * 8} Eur)</td>
                             </tr>`).join('')}
                     </tbody>
                 </table>
